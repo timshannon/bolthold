fd4ece2cb82da1f4d7236279139f152a90c6947b
Tim Shannon <shannon.timothy@gmail.com>
2016-10-26 22:28:09 -0500

    Changed Find tests to be table driven
    Just need to write them now, so far so good

--- find_test.go
+++ find_test.go
@@ -44,101 +44,146 @@ func (i *ItemTest) equal(other *ItemTest) bool {
 }
 
 var testData = []ItemTest{
-	ItemTest{
+	ItemTest{ //0
 		ID:       0,
 		Name:     "car",
 		Category: "vehicle",
 		Created:  time.Now().AddDate(-1, 0, 0),
 	},
-	ItemTest{
+	ItemTest{ //1
 		ID:       1,
 		Name:     "truck",
 		Category: "vehicle",
 		Created:  time.Now().AddDate(0, 30, 0),
 	},
-	ItemTest{
+	ItemTest{ //2
 		ID:       2,
 		Name:     "seal",
 		Category: "animal",
 		Created:  time.Now().AddDate(-1, 0, 0),
 	},
-	ItemTest{
+	ItemTest{ //3
 		ID:       3,
 		Name:     "van",
 		Category: "vehicle",
 		Created:  time.Now().AddDate(0, 30, 0),
 	},
-	ItemTest{
+	ItemTest{ //4
 		ID:       8,
 		Name:     "pizza",
 		Category: "food",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //5
 		ID:       1,
 		Name:     "crow",
 		Category: "animal",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //6
 		ID:       5,
 		Name:     "van",
 		Category: "vehicle",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //7
 		ID:       5,
 		Name:     "pizza",
 		Category: "food",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //8
 		ID:       6,
 		Name:     "lion",
 		Category: "animal",
 		Created:  time.Now().AddDate(3, 0, 0),
 	},
-	ItemTest{
+	ItemTest{ //9
 		ID:       7,
 		Name:     "bear",
 		Category: "animal",
 		Created:  time.Now().AddDate(3, 0, 0),
 	},
-	ItemTest{
+	ItemTest{ //10
 		ID:       9,
 		Name:     "tacos",
 		Category: "food",
 		Created:  time.Now().AddDate(-3, 0, 0),
 	},
-	ItemTest{
+	ItemTest{ //11
 		ID:       10,
 		Name:     "golf cart",
 		Category: "vehicle",
 		Created:  time.Now().AddDate(0, 0, 30),
 	},
-	ItemTest{
+	ItemTest{ //12
 		ID:       11,
 		Name:     "oatmeal",
 		Category: "food",
 		Created:  time.Now().AddDate(0, 0, -30),
 	},
-	ItemTest{
+	ItemTest{ //13
 		ID:       8,
 		Name:     "mouse",
 		Category: "animal",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //14
 		ID:       12,
 		Name:     "fish",
 		Category: "animal",
 		Created:  time.Now(),
 	},
-	ItemTest{
+	ItemTest{ //15
 		ID:       13,
 		Name:     "fish",
 		Category: "food",
 		Created:  time.Now(),
+	},
+	ItemTest{ //16
+		ID:       9,
+		Name:     "zebra",
+		Category: "animal",
+		Created:  time.Now(),
+	},
+}
+
+type test struct {
+	name   string
+	query  *bolthold.Query
+	result []int // indices of test data to be found
+}
+
+var tests = []test{
+	test{
+		name:   "Equal Key",
+		query:  bolthold.Where(bolthold.Key()).Eq(testData[4].key()),
+		result: []int{4},
+	},
+	test{
+		name:   "Equal Field Without Index",
+		query:  bolthold.Where("Name").Eq(testData[1].Name),
+		result: []int{1},
+	},
+	test{
+		name:   "Equal Field With Index",
+		query:  bolthold.Where("Category").Eq("vehicle"),
+		result: []int{0, 1, 3, 6, 11},
+	},
+	test{
+		name:   "Greater Than Key",
+		query:  bolthold.Where(bolthold.Key()).Gt(testData[10].key()),
+		result: []int{16},
+	},
+	test{
+		name:   "Greater Than Field Without Index",
+		query:  bolthold.Where("ID").Gt(10),
+		result: []int{12, 14, 15},
+	},
+	test{
+		name:   "Greater Than Field With Index",
+		query:  bolthold.Where("Category").Gt("food"),
+		result: []int{0, 1, 3, 6, 11},
 	},
 }
 
@@ -151,77 +196,35 @@ func insertTestData(t *testing.T, store *bolthold.Store) {
 	}
 }
 
-func TestFindEqualKey(t *testing.T) {
+func TestFind(t *testing.T) {
 	testWrap(t, func(store *bolthold.Store, t *testing.T) {
 		insertTestData(t, store)
 
-		want := testData[4]
+		for _, tst := range tests {
+			t.Run(tst.name, func(t *testing.T) {
+				var result []ItemTest
 
-		var result []ItemTest
+				err := store.Find(&result, tst.query)
+				if err != nil {
+					t.Fatalf("Error finding data from bolthold: %s", err)
+				}
+				if len(result) != len(tst.result) {
+					t.Fatalf("Find result count is %d wanted %d.  Results: %v", len(result), len(tst.result), result)
+				}
 
-		err := store.Find(&result, bolthold.Where(bolthold.Key()).Eq(want.key()))
+				for i := range result {
+					found := false
+					for k := range tst.result {
+						if result[i].equal(&testData[tst.result[k]]) {
+							found = true
+						}
+					}
 
-		if err != nil {
-			t.Fatalf("Error finding data from bolthold: %s", err)
-		}
-
-		if len(result) != 1 {
-			t.Fatalf("Find result count is %d wanted %d", len(result), 1)
-		}
-
-		if !result[0].equal(&want) {
-			t.Fatalf("Got %v wanted %v.", result[0], want)
-		}
-	})
-}
-
-func TestFindEqualFieldNoIndex(t *testing.T) {
-	testWrap(t, func(store *bolthold.Store, t *testing.T) {
-		insertTestData(t, store)
-
-		want := testData[1]
-
-		var result []ItemTest
-
-		err := store.Find(&result, bolthold.Where("Name").Eq(want.Name))
-
-		if err != nil {
-			t.Fatalf("Error finding data from bolthold: %s", err)
-		}
-
-		if len(result) != 1 {
-			t.Fatalf("Find result count is %d wanted %d", len(result), 1)
-		}
-
-		if !result[0].equal(&want) {
-			t.Fatalf("Got %v wanted %v.", result[0], want)
-		}
-
-	})
-}
-
-func TestFindEqualFieldIndex(t *testing.T) {
-	testWrap(t, func(store *bolthold.Store, t *testing.T) {
-		insertTestData(t, store)
-
-		var result []ItemTest
-
-		cat := "vehicle"
-
-		err := store.Find(&result, bolthold.Where("Category").Eq(cat))
-
-		if err != nil {
-			t.Fatalf("Error finding data from bolthold: %s", err)
-		}
-
-		if len(result) != 5 {
-			t.Fatalf("Find result count is %d wanted %d", len(result), 5)
-		}
-
-		for i := range result {
-			if result[i].Category != cat {
-				t.Fatalf("Result %s is not of the category %s", result[i].key(), cat)
-			}
+					if !found {
+						t.Fatalf("Could not find %v in the result set! Full results: %v", result[i], result)
+					}
+				}
+			})
 		}
 	})
 }
