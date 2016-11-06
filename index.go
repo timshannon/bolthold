77e1bbd91bfe1714e983a40a224ef3680f7b9980
Tim Shannon <shannon.timothy@gmail.com>
2016-11-06 13:52:04 -0600

    Fixed issue with iterator critera not being correct...
    when a new index is selected.

--- index.go
+++ index.go
@@ -164,12 +164,11 @@ func newIterator(tx *bolt.Tx, typeName string, query *Query) *iterator {
 		return iter
 	}
 
-	criteria := query.fieldCriteria[query.index]
-
 	// 3 scenarios
 	//   Key field
 	if query.index == Key() {
 		iter.indexCursor = tx.Bucket([]byte(typeName)).Cursor()
+		criteria := query.fieldCriteria[Key()]
 
 		iter.nextKeys = func(prepCursor bool, cursor *bolt.Cursor) ([][]byte, error) {
 			var nKeys [][]byte
@@ -237,6 +236,7 @@ func newIterator(tx *bolt.Tx, typeName string, query *Query) *iterator {
 
 	iter.nextKeys = func(prepCursor bool, cursor *bolt.Cursor) ([][]byte, error) {
 		var nKeys [][]byte
+		criteria := query.fieldCriteria[query.index]
 
 		for len(nKeys) < iteratorKeyMinCacheSize {
 			var k, v []byte
