64584f4586a2298d6b862640f23e57c74f3e0ae4
Tim Shannon <shannon.timothy@gmail.com>
2016-11-06 22:25:24 -0600

    Fixed typo

--- doc.go
+++ doc.go
@@ -1,11 +1,11 @@
 /*
-Package bolthold is an indexing and querying layer on top of a Bolt DB.  The goal is to allow easy, persistant storage
+Package bolthold is an indexing and querying layer on top of a Bolt DB.  The goal is to allow easy, persistent storage
 and retrieval of Go types.  BoltDB is an embedded key-value store, and bolthold servers a similar use case however with
 a higher level interface for common uses of BoltDB.
 
 Go Types
 
-BoltHold deals directly with Go Types.  When inserting data, you pass in your stucture directly.  When querying data you
+BoltHold deals directly with Go Types.  When inserting data, you pass in your structure directly.  When querying data you
 pass in a pointer to a slice of the type you want to return.  By default Gob encoding is used. You can put multiple
 different types into the same DB file and they (and their indexes) will be stored separately.
 
