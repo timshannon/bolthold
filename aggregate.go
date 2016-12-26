e924c9dbefdec22073e3433ee1ca2f8579ac1de7
Tim Shannon <tim@townsourced.com>
2016-12-25 22:26:09 -0600

    Fleshing out how I'll be handling aggregates, and Min/Max

--- aggregate.go
+++ aggregate.go
@@ -14,29 +14,58 @@ type AggregateResult struct {
 }
 
 // Group returns the field grouped by in the query
-func (a *AggregateResult) Group(result interface{}) error {
-	result = a.group.Interface()
-	return nil
+func (a *AggregateResult) Group(result interface{}) {
+	resultVal := reflect.ValueOf(result)
+	if resultVal.Kind() != reflect.Ptr {
+		panic("result argument must be an address")
+	}
+
+	resultVal.Elem().Set(a.group)
 }
 
 // Reduction is the collection of records that are part of the AggregateResult Group
-func (a *AggregateResult) Reduction(result interface{}) error {
-	return errors.New("TODO")
+func (a *AggregateResult) Reduction(result interface{}) {
+	resultVal := reflect.ValueOf(result)
+
+	if resultVal.Kind() != reflect.Ptr || resultVal.Elem().Kind() != reflect.Slice {
+		panic("result argument must be a slice address")
+	}
+
+	sliceVal := resultVal.Elem()
+
+	for i := range a.reduction {
+		sliceVal = reflect.Append(sliceVal, a.reduction[i])
+	}
+
+	resultVal.Elem().Set(sliceVal.Slice(0, sliceVal.Len()))
+}
+
+func (a *AggregateResult) sort(field string) {
+	//TODO: Sort
 }
 
 // Max Returns the maxiumum value of the Aggregate Grouping
-func (a *AggregateResult) Max(field string, result interface{}) error {
-	return errors.New("TODO")
+func (a *AggregateResult) Max(field string, result interface{}) {
+	a.sort(field)
+
+	resultVal := reflect.ValueOf(result)
+	if resultVal.Kind() != reflect.Ptr {
+		panic("result argument must be an address")
+	}
+
+	resultVal.Elem().Set(a.reduction[:len(a.reduction)-1][0])
 }
 
 // Min returns the minimum value of the Aggregate Grouping
-func (a *AggregateResult) Min(field string, result interface{}) error {
-	return errors.New("TODO")
-}
+func (a *AggregateResult) Min(field string, result interface{}) {
+	a.sort(field)
 
-// Median returns the median record of the aggregate grouping
-func (a *AggregateResult) Median(result interface{}) error {
-	return errors.New("TODO")
+	resultVal := reflect.ValueOf(result)
+	if resultVal.Kind() != reflect.Ptr {
+		panic("result argument must be an address")
+	}
+
+	resultVal.Elem().Set(a.reduction[0])
 }
 
 // Avg returns the average value of the aggregate grouping
@@ -45,14 +74,13 @@ func (a *AggregateResult) Avg(field string) (float64, error) {
 }
 
 // Count returns the number of records in the aggregate grouping
-func (a *AggregateResult) Count() int64 {
-	return -1
+func (a *AggregateResult) Count() int {
+	return len(a.reduction)
 }
 
 // FindAggregate returns an aggregate grouping for the passed in query
 // groupBy is optional
 func (s *Store) FindAggregate(dataType interface{}, query *Query, groupBy string) ([]*AggregateResult, error) {
-
 	var result []*AggregateResult
 	var err error
 	err = s.Bolt().View(func(tx *bolt.Tx) error {
